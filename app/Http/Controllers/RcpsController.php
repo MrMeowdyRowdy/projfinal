cb376ad532d7b521f917b15c384c8404429b90ba
Dennis on Devserver <dennisocana@gmail.com>
2023-12-17 06:20:52 +0000

    fix rcp

--- app/Http/Controllers/RcpsController.php
+++ app/Http/Controllers/RcpsController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\StoreRCPRequest;
-use App\Http\Requests\UpdateRCPRequest;
+use App\Http\Requests\StoreRcpRequest;
+use App\Http\Requests\UpdateRcpRequest;
 use App\Models\Rcp;
 
 class RcpsController extends Controller
@@ -16,7 +16,7 @@ class RcpsController extends Controller
      */
     public function index() 
     {
-        $rcps = RCP::latest()->paginate(10);
+        $rcps = Rcp::latest()->paginate(10);
 
         return view('rcps.index', compact('rcps'));
     }
@@ -34,12 +34,12 @@ class RcpsController extends Controller
     /**
      * Store a newly created rcp
      * 
-     * @param RCP $rcp
-     * @param StoreRCPRequest $request
+     * @param Rcp $rcp
+     * @param StoreRcpRequest $request
      * 
      * @return \Illuminate\Http\Response
      */
-    public function store(RCP $rcp, StoreRCPRequest $request) 
+    public function store(Rcp $rcp, StoreRcpRequest $request) 
     {
         //For demo purposes only. When creating rcp or inviting a rcp
         // you should create a generated random password and email it to the rcp
@@ -48,17 +48,17 @@ class RcpsController extends Controller
         ]));
 
         return redirect()->route('rcps.index')
-            ->withSuccess(__('RCP registrada correctamente.'));
+            ->withSuccess(__('Rcp registrada correctamente.'));
     }
 
     /**
      * Show rcp data
      * 
-     * @param RCP $rcp
+     * @param Rcp $rcp
      * 
      * @return \Illuminate\Http\Response
      */
-    public function show(RCP $rcp) 
+    public function show(Rcp $rcp) 
     {
         return view('rcps.show', [
             'rcp' => $rcp
@@ -68,11 +68,11 @@ class RcpsController extends Controller
     /**
      * Edit rcp data
      * 
-     * @param RCP $rcp
+     * @param Rcp $rcp
      * 
      * @return \Illuminate\Http\Response
      */
-    public function edit(RCP $rcp) 
+    public function edit(Rcp $rcp) 
     {
 
         return view('rcps.edit', [
@@ -82,31 +82,31 @@ class RcpsController extends Controller
     /**
      * Update rcp data
      * 
-     * @param RCP $rcp
-     * @param UpdateRCPRequest $request
+     * @param Rcp $rcp
+     * @param UpdateRcpRequest $request
      * 
      * @return \Illuminate\Http\Response
      */
-    public function update(RCP $rcp, UpdateRCPRequest $request) 
+    public function update(Rcp $rcp, UpdateRcpRequest $request) 
     {
         $rcp->update($request->validated());
 
         return redirect()->route('rcps.index')
-            ->withSuccess(__('RCP actualizada correctamente.'));
+            ->withSuccess(__('Rcp actualizada correctamente.'));
     }
 
     /**
      * Delete rcp data
      * 
-     * @param RCP $rcp
+     * @param Rcp $rcp
      * 
      * @return \Illuminate\Http\Response
      */
-    public function destroy(RCP $rcp) 
+    public function destroy(Rcp $rcp) 
     {
         $rcp->delete();
 
         return redirect()->route('rcps.index')
-            ->withSuccess(__('RCP eliminada correctamente.'));
+            ->withSuccess(__('Rcp eliminada correctamente.'));
     }
 }
--- app/Models/Rcp.php
+++ app/Models/Rcp.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class RCP extends Model
+class Rcp extends Model
 {
     use HasFactory;
 
