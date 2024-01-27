0c2973eea0d47abeb4ef7458f3756d10870099e6
Dennis on Devserver <dennisocana@gmail.com>
2024-01-27 02:16:13 +0000

    cambio de funcionalidad api

--- app/Http/Controllers/Api/UsersApiController.php
+++ app/Http/Controllers/Api/UsersApiController.php
@@ -3,13 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
-use Illuminate\Http\Request;
+
+use App\Models\Llamada;
+
+use Illuminate\Support\Carbon;
 
 class UsersApiController extends Controller
 {
     public function index()
     {
-        return User::all();
+        $llamadas = Llamada::select('llamadas.id','llamadas.interpreterID','llamadas.horaInicio','llamadas.horaFin','empresa_clientes.nombre','proveedors.nombre','lengua_l_e_p_s.lengua','categorias.categoria')
+        ->join('empresa_clientes', 'llamadas.empresaCliente', '=', 'empresa_clientes.id')
+        ->join('proveedors', 'llamadas.proveedor', '=', 'proveedors.id')
+        ->join('lengua_l_e_p_s', 'llamadas.lenguaLEP', '=', 'lengua_l_e_p_s.id')
+        ->join('categorias', 'llamadas.tipo', '=', 'categorias.id')
+        ->get();
+        foreach ($llamadas as $llamada) {
+            $llamada->duracion = Carbon::parse($llamada->horaInicio)->diffInMinutes(Carbon::parse($llamada->horaFin));
+        }
+        return $llamadas;
     }
 }
